37a559fdd7972690e6c629907d2593d429cc9056
egorherby <rukin.egor@gmail.com>
2022-05-11 00:03:20 +0300

    replaced logreg with knn, added 2 feature selections

--- src/forest_cover_pred/models/pipeline.py
+++ src/forest_cover_pred/models/pipeline.py
@@ -1,11 +1,17 @@
-from sklearn.pipeline import Pipeline
+from sklearn import model_selection
+from sklearn.pipeline import make_pipeline
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.preprocessing import StandardScaler, QuantileTransformer
 
 
-def create_pipe(select_model, random_state, use_scaler, logreg_penalty,
-        logreg_max_iter, logreg_c, tree_crit, tree_max_depth, tree_min_samples_leaf):
+def create_pipe(
+    select_model, random_state, use_scaler, feature_eng_type,
+    pca_n_features, var_threshold,
+    knn_neighbors, knn_weights, tree_crit,
+    tree_max_depth, tree_min_samples_leaf):
     # param_grid = {
     #     "tree__criterion":["gini", "entropy"],
     #     "tree__splitter":["best", "random"],
@@ -15,17 +21,24 @@ def create_pipe(select_model, random_state, use_scaler, logreg_penalty,
     # }
     if tree_max_depth == 0:
         tree_max_depth = None
-    scaler = [("scaler", StandardScaler())]
+    scaler = {
+        'none': 'passthrough',
+        "quantile": QuantileTransformer(output_distribution='normal'),
+        "standard": StandardScaler(),
+        }
+    feature_eng = {
+        'none': "passthrough",
+        "pca": PCA(pca_n_features),
+        "var": VarianceThreshold(threshold=var_threshold)
+        }
     models = {
-        "logreg":[
-            ("log", LogisticRegression(penalty=logreg_penalty, C=logreg_c, max_iter=logreg_max_iter))
-        ],
-        "tree":[
-            ("tree", DecisionTreeClassifier(random_state=random_state, criterion=tree_crit, max_depth=tree_max_depth, min_samples_leaf=tree_min_samples_leaf))
-        ]
-    }
-    steps = models[select_model]
-    if use_scaler:
-        steps = scaler + steps
-    return Pipeline(steps)
+        "knn": KNeighborsClassifier(n_neighbors=knn_neighbors, weights=knn_weights, n_jobs=-1),
+        "tree": DecisionTreeClassifier(
+            random_state=random_state, criterion=tree_crit, 
+            max_depth=tree_max_depth, min_samples_leaf=tree_min_samples_leaf)
+        }
+    if feature_eng_type=="var":
+        return make_pipeline(feature_eng[feature_eng_type], scaler[use_scaler], models[select_model])
+    else:
+        return make_pipeline(scaler[use_scaler], feature_eng[feature_eng_type], models[select_model])
 
