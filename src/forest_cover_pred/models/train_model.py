f7b9242355ac8a3544cb2ceb1d588cb3b23ab643
egorherby <rukin.egor@gmail.com>
2022-05-11 00:04:36 +0300

    changed options for knn, fixes here and there

--- src/forest_cover_pred/models/train_model.py
+++ src/forest_cover_pred/models/train_model.py
@@ -32,44 +32,53 @@ from sklearn.model_selection import RandomizedSearchCV, cross_validate, Stratifi
     show_default=True,
 )
 @click.option("--random-state", default=42, show_default=True)
-@click.option("--select-model", default="tree", type=click.Choice(["tree", "logreg"], case_sensitive=False), 
+@click.option("--select-model", default="tree", type=click.Choice(["tree", "knn"], case_sensitive=False), 
     show_default=True)
-@click.option("--use-scaler", default=True, type=bool, show_default=True)
-@click.option("--logreg-max-iter", default=50, type=float, show_default=True)
-@click.option("--logreg-penalty", default="l2", type=click.Choice(["none", "l2"]), show_default=True)
-@click.option("--logreg-c", default=1, type=click.FloatRange(min=0, min_open=True), show_default=True)
+@click.option("--use-scaler", default='standard', type=click.Choice(["none", "standard", "quantile"]),
+    show_default=True)
+@click.option("--feature-eng-type", default='none', type=click.Choice(["none",'var', "pca"]),
+    show_default=True)
+@click.option("--var-threshold", default=1e-3, show_default=True)
+@click.option("--pca-n-features", default=40, show_default=True)
+@click.option("--knn-neighbors", default=10, show_default=True)
+@click.option("--knn-weights", default='uniform', type=click.Choice(["uniform", "distance"]),
+    show_default=True)
 @click.option("--tree-crit", default="gini", type=click.Choice(["gini", "entropy"], case_sensitive=False), 
     show_default=True)
 @click.option("--tree-max-depth", default=0, type=int, show_default=True)
 @click.option("--tree-min-samples-leaf", default=1, type=int, show_default=True)
 def train(
     dataset_path, save_model_path, test_split_ratio,
-    random_state, select_model, use_scaler, 
-    logreg_max_iter, logreg_penalty,logreg_c, tree_crit,
-    tree_max_depth,tree_min_samples_leaf):
+    select_model, random_state, use_scaler, feature_eng_type,
+    pca_n_features, var_threshold,
+    knn_neighbors, knn_weights, tree_crit,
+    tree_max_depth, tree_min_samples_leaf):
     X_tr, X_val, y_tr, y_val = get_data(dataset_path, test_split_ratio, random_state)
     with mlflow.start_run():
-        pipe = create_pipe(select_model, random_state, use_scaler, logreg_penalty,
-            logreg_max_iter, logreg_c, tree_crit, tree_max_depth, tree_min_samples_leaf)
+        pipe = create_pipe(
+            select_model, random_state, use_scaler,
+            feature_eng_type,pca_n_features, var_threshold,
+            knn_neighbors, knn_weights, tree_crit,
+            tree_max_depth, tree_min_samples_leaf)
         scorings = ("accuracy", "f1_macro", "roc_auc_ovr")
-        tracking_params = {"model":select_model, "random_state":random_state,
-            "use_scaler":use_scaler, "logreg_penalty":logreg_penalty,
-            "logreg_max_iter":logreg_max_iter, "logreg_c":logreg_c, "tree_crit":tree_crit,
-            "tree_max_depth":tree_max_depth, "tree_min_samples_leaf":tree_min_samples_leaf}
+        tracking_params = {
+            "select_model":select_model, "random_state":random_state,
+            "use_scaler":use_scaler,"feature_eng_type":feature_eng_type,
+            "pca_n_features":pca_n_features, "var_threshold":var_threshold,
+            "knn_neighbors":knn_neighbors, "knn_weights":knn_weights, 
+            "tree_crit":tree_crit, "tree_max_depth":tree_max_depth, 
+            "tree_min_samples_leaf":tree_min_samples_leaf}
         mlflow.log_params(tracking_params)
         # outer_cv = StratifiedKFold(n_splits=3, shuffle=True, random_state=random_state)
         inner_cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=random_state)
         # clf = RandomizedSearchCV(pipe, param_grid, cv=inner_cv, n_iter=100, n_jobs=-1)
         # nested_cv = cross_validate(pipe, X_tr, y_tr, cv=outer_cv, n_jobs=-1, scoring=scorings)
         cv = cross_validate(pipe, X_tr, y_tr, cv=inner_cv, n_jobs=-1, scoring=scorings)
-        # click.echo(f"nested scores {metric}={nested_score.mean()}")
-        mlflow.log_metrics({
+        cv_average_metrics = {
             "accuracy":cv["test_accuracy"].mean(), 
             "f1_score":cv["test_f1_macro"].mean(),
-            "roc_auc_ovr":cv["test_roc_auc_ovr"].mean()})
-        print({
-            "accuracy":cv["test_accuracy"].mean(), 
-            "f1_score":cv["test_f1_macro"].mean(),
-            "roc_auc_ovr":cv["test_roc_auc_ovr"].mean()})
+            "roc_auc_ovr":cv["test_roc_auc_ovr"].mean()}
+        click.echo(f"cv averaged metrics {cv_average_metrics}")
+        mlflow.log_metrics(cv_average_metrics)
         # dump(pipe, save_model_path)
         # click.echo(f"model saved to {save_model_path}")
